9fbd889d69d19961bf9cd50fd9808501824babdf
maqilong <84676088+maqilong-lab@users.noreply.github.com>
2022-03-17 00:01:59 +0800

    简单控制流水灯2

--- 初学单片机/3-14独立按键控制流水灯系列操作/main.c
+++ 初学单片机/3-14独立按键控制流水灯系列操作/main.c
@@ -1,16 +1,32 @@
 #include <REGX52.H>
 
+void Delay1ms(xms)	
+{
+	unsigned char i, j;
+	while(xms)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+}
+
+
 void main()
 {
 	while(1)
 	{
-		if(P3_0 ==0 || P3_1 == 0)
-	    {
-			P1=0xFE;
-		}
-		else
+		if(P3_0 == 0)
 		{
-			P1=0xFF;
+			Delay1ms(20);
+			while(P3_0 == 1);
+			Delay1ms(20);
+			
+			P1_0 =~ P1_0;
 		}
 	}
 }
