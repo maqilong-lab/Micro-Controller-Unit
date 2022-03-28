8cfa115f093cbdc5da8d7c2fe16be7e9264d1ef9
maqilong <84676088+maqilong-lab@users.noreply.github.com>
2022-03-28 23:48:06 +0800

    蜂鸣器播放按键提示音

---
+++ 初学单片机/3-28蜂鸣器播放提示音/main.c
@@ -0,0 +1,20 @@
+#include <REGX52.H>
+#include "Key.h"
+#include "Nixie.h"
+#include "Buzzer.h"
+
+unsigned char KeyNum;
+
+void main( )
+{
+	Nixie(1,0);
+	while(1)
+	{
+		KeyNum = Key();
+		if(KeyNum)
+		{
+			Buzzer_Time(1000);
+			Nixie(1,KeyNum);
+		}
+	}
+}
